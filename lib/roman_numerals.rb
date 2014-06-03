a8d4f2ba0a12c0297d1702e096f9077a16e44688
Colin Frankish <colin.frankish@gmail.com>
2014-06-03 12:12:18 +0100

    constant used to cover all numnbers, but needs refining

--- lib/roman_numerals.rb
+++ lib/roman_numerals.rb
@@ -1,8 +1,9 @@
 class RomanNumerals
-  #ROMAN_NUMERALS = {10: "X",5: "V",1: "I"}
-
+  ROMAN_NUMERALS = {10 => "X", 5 => "V", 1 => "I"}
+#setting the constant ROMAN_NUMERALS allows you to
+#call it in the method, pass the argument as key, so
+#it will always return the value. 
   def self.convert(number)
-    return "V" if number == 5
-    "I"
+    ROMAN_NUMERALS[number]
   end
 end
